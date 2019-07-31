67cb32b66a58ef05fcccd6fbaa5cec9ae9bace88
Nikita Litvin <n@deltaidea.com>
2019-07-31 15:26:53 +0500

    feat(map_render): tone down the amount of logs

--- map_render_remote_script.sh
+++ map_render_remote_script.sh
@@ -2,31 +2,34 @@
 
 # This script is used by ./map_render.py. Don't call this script directly!
 
-echo "Building Overviewer from source..."
 cd ~
-sudo yum update -y
-sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum install -y gcc python36 python36-devel python36-pip python36-pillow python36-pillow-devel python36-numpy
-wget -O overviewer-source.tar.gz https://github.com/overviewer/Minecraft-Overviewer/archive/1b85e478f55932aad8312fb49daa818e9b258b05.tar.gz
-tar xvzf overviewer-source.tar.gz
-rm overviewer-source.tar.gz
+echo "Updating OS packages..."
+sudo yum update -y > /dev/null
+echo "Installing Python to compile Overviewer..."
+sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm > /dev/null
+sudo yum install -y gcc python36 python36-devel python36-pip python36-pillow python36-pillow-devel python36-numpy > /dev/null
+echo "Downloading Overviewer..."
+wget -O overviewer-source.tar.gz https://github.com/overviewer/Minecraft-Overviewer/archive/1b85e478f55932aad8312fb49daa818e9b258b05.tar.gz > /dev/null
+tar xvzf overviewer-source.tar.gz > /dev/null
+rm overviewer-source.tar.gz > /dev/null
 mv Minecraft-Overviewer* overviewer
 cd ~/overviewer
-python3 setup.py build
+echo "Compiling Overviewer..."
+python3 setup.py build > /dev/null
 
 echo "Downloading the Minecraft client for textures..."
-wget -O client.jar https://overviewer.org/textures/1.14.4
+wget -O client.jar https://overviewer.org/textures/1.14.4 > /dev/null
 
 # These variables are used in config.py, see https://github.com/dq-server/overviewer-config
 export MINECRAFT_WORLD_DIR="~/world-backup"
 export MINECRAFT_MAP_DIR="~/overviewer/map"
 export MINECRAFT_CLIENT_PATH="~/overviewer/client.jar"
 
-echo "Downloading latest map config and icons from github.com/dq-server/overviewer-config..."
+echo "Downloading map config and icons from github.com/dq-server/overviewer-config..."
 cd ~/overviewer
-wget -O config.tar.gz https://github.com/dq-server/overviewer-config/tarball/master
-tar xvzf config.tar.gz
-rm config.tar.gz
+wget -O config.tar.gz https://github.com/dq-server/overviewer-config/tarball/master > /dev/null
+tar xvzf config.tar.gz > /dev/null
+rm config.tar.gz > /dev/null
 mv dq-server-overviewer-config* config
 
 echo "Rendering the map..."
