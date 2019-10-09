fac34d6c172f1e2bbf02235bfcc403725b950af0
Nikita Litvin <n@deltaidea.com>
2019-10-10 02:22:43 +0300

    fix(management_api): fix typo in logfile assignment

--- management_api_start.sh
+++ management_api_start.sh
@@ -4,5 +4,5 @@
 
 # Requires sude to read /etc/letsencrypt/live/minecraft.deltaidea.com/*.pem
 sudo screen -dmS management_api /home/ec2-user/scripts/management_api.py
-sudo screen -S map -X logfile /home/ec2-user/management_api_process.log
-sudo screen -S map -X log
+sudo screen -S management_api -X logfile /home/ec2-user/management_api_process.log
+sudo screen -S management_api -X log
